e3a20f5489302423d7a00bfaceb9d7291ec4a2b0
Barath <127286391+ItsBarathr@users.noreply.github.com>
2023-07-12 14:46:08 +0530

    Add files via upload

---
+++ config/db_config.php
@@ -0,0 +1,24 @@
+<?php
+// $hostname = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "u799942438_isjesm";//change db name
+
+// $conn = mysqli_connect($hostname, $username, $password, $database) or die("Database connection failed");
+
+// $base_url = "https://refer4rich.com/";
+// $my_email = "refer4rich@refer4rich.com";
+?>
+
+<?php
+$hostname = "localhost";
+$username = "u799942438_journal";
+$password = "Isjesm200@";
+$database = "u799942438_isjesm";//change db name
+
+$conn = mysqli_connect($hostname, $username, $password, $database) or die("Database connection failed");
+
+$base_url = "https://isjesm.com/user/";
+$my_email = "support@isjesm.com";
+
+?>
